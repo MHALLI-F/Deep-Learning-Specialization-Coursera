d5a2d0081cb7a5c66acd7b8bdcb3bcb8b8e116b9
MHALLI-F <71670367+MHALLI-F@users.noreply.github.com>
2020-10-25 03:37:01 +0100

    Update neural_network _one_hidden_layer.py

--- Neural Networks and Deep Learning/week3/neural_network_one_hidden_layer.py
+++ Neural Networks and Deep Learning/week3/neural_network_one_hidden_layer.py
@@ -173,16 +173,18 @@ def backward_propagation(parameters, cache, X, Y):
     return grads
 
 
-def update_parameters(parameters, grads, learning_rate=1.2):
+# GRADED FUNCTION: update_parameters
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
     """
     Updates parameters using the gradient descent update rule given above
-
+    
     Arguments:
-    parameters -- python dictionary containing your parameters
-    grads -- python dictionary containing your gradients
-
+    parameters -- python dictionary containing your parameters 
+    grads -- python dictionary containing your gradients 
+    
     Returns:
-    parameters -- python dictionary containing your updated parameters
+    parameters -- python dictionary containing your updated parameters 
     """
     # Retrieve each parameter from the dictionary "parameters"
     ### START CODE HERE ### (≈ 4 lines of code)
@@ -191,38 +193,29 @@ def update_parameters(parameters, grads, learning_rate=1.2):
     W2 = parameters['W2']
     b2 = parameters['b2']
     ### END CODE HERE ###
-
+    
     # Retrieve each gradient from the dictionary "grads"
     ### START CODE HERE ### (≈ 4 lines of code)
     dW1 = grads['dW1']
-    db1 = grads['db1']
+    db1 = grads['db2']
     dW2 = grads['dW2']
     db2 = grads['db2']
     ## END CODE HERE ###
-
+    
     # Update rule for each parameter
     ### START CODE HERE ### (≈ 4 lines of code)
-    W1 = None
-    b1 = None
-    W2 = None
-    b2 = None
+    W1 = W1-learning_rate*dW1
+    b1 = b1-learning_rate*db1
+    W2 = W2-learning_rate*dW2
+    b2 = b2-learning_rate*dW1
     ### END CODE HERE ###
-
-    # Update rule for each parameter
-    ### START CODE HERE ### (≈ 4 lines of code)
-    W1 = W1 - learning_rate*dW1
-    b1 = b1 - learning_rate*db1
-    W2 = W2 - learning_rate*dW2
-    b2 = b2 - learning_rate*db2
-    ### END CODE HERE ###
-
+    
     parameters = {"W1": W1,
                   "b1": b1,
                   "W2": W2,
                   "b2": b2}
-
+    
     return parameters
-
 
 def main():
 
